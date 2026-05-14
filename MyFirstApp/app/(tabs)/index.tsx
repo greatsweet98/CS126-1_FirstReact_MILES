f8fe56cc46360eb997f2b549b41fa0d9243444e1
greatsweet98 <mustacheguyjmgaming@gmail.com>
2026-05-14 12:09:44 +0800

    change into interactive profile card

--- MyFirstApp/app/(tabs)/index.tsx
+++ MyFirstApp/app/(tabs)/index.tsx
@@ -1,26 +1,46 @@
-import { Image } from 'expo-image';
-import { StyleSheet } from 'react-native';
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, Text, TextInput, Button, Image, ScrollView, StyleSheet } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('');
+  const [count, setCount] = useState(0);
+
+  const message =
+    count > 0
+      ? `${name}, you tapped ${count} times!`
+      : "Tap the + button to start";
+
   return (
-      <View style={styles.screen}>
+      <ScrollView contentContainerStyle={styles.container}>
         <Image
           source={{ uri: 'https://i1-c.pinimg.com/1200x/cc/52/e6/cc52e67593f815b1ca4d174e40ac83ef.jpg' }}
           style={styles.photo}
         />
-        <Text style={styles.name}>Jacob Matthew D. Miles</Text>
-        <Text style={styles.bio}>3Y MMA Student - CS126-1</Text>
-        <Text style={styles.description}>Cosplayer, artist, writer, & advocate</Text>
-      </View>
+        <TextInput placeholder="Enter your name" onChangeText={setName} style={styles.input}/>
+        <Text style={styles.text}>{name === '' ? "Please enter your name" : `Hello, ${name}!`}</Text>
+        <Text style={styles.text}>{message}</Text>
+        <View style={styles.buttonContainer}>
+          <Button title="+" onPress={() => setCount(count + 1)} />
+          <Button title="-" onPress={() => setCount(count - 1)} />
+          <Button title="Reset" onPress={() => setCount(0)} />
+        </View>
+      </ScrollView>
   );
 }
 
 //On 11:32AM on May 14, I changed the name, bio, pfp, and added a short desc
 const styles = StyleSheet.create({
+  container: {
+    flex: 1, alignItems: 'center', justifyContent: 'center', padding: 20
+  },
   screen: {flex:1, alignItems:'center', justifyContent:'center'},
-  photo: {width:120, height:120, borderRadius:60},
-  name: {fontSize:30, fontWeight:'bold', marginTop:12, color:'#d7c903'},
-  bio: {fontSize:18, color:'#ff8400'},
-  description: {fontSize:13, color: '#9f9f9f'}
+  photo: {width:120, height:120, borderRadius:60, marginBottom:20},
+  text: {fontSize: 16, marginVertical: 5},
+  buttonContainer: {marginTop: 10, width: '60%'},
+  input: {
+    borderWidth: 1,
+    width: '80%',
+    padding: 10,
+    marginBottom: 10,
+  }
 });
